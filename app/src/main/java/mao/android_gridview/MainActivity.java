6df6da2203551ac04d50ad28f2f1c9640bef0913
mao <1296193245@qq.com>
2022-10-04 12:38:28 +0800

    android_GridView

--- app/src/main/java/mao/android_gridview/MainActivity.java
+++ app/src/main/java/mao/android_gridview/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                startActivity(new Intent(MainActivity.this, MainActivity5.class));
             }
         });
 
