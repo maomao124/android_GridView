96873a916bdeee72684a8e0261dc55fc08dfcfe6
mao <1296193245@qq.com>
2022-10-04 11:16:11 +0800

    android_GridView

--- app/src/main/java/mao/android_gridview/MainActivity2.java
+++ app/src/main/java/mao/android_gridview/MainActivity2.java
@@ -1,9 +1,14 @@
 package mao.android_gridview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,5 +41,48 @@ public class MainActivity2 extends AppCompatActivity
         GridViewAdapter gridViewAdapter = new GridViewAdapter(this, list);
 
         gridView.setAdapter(gridViewAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                toastShow("第" + (position + 1) + "个被点击了");
+            }
+        });
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
+        {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id)
+            {
+                new AlertDialog.Builder(MainActivity2.this)
+                        .setTitle("删除提示")
+                        .setMessage("是否删除第" + (position + 1) + "个？")
+                        .setPositiveButton("确定删除", new DialogInterface.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which)
+                            {
+                                list.remove(position);
+                                gridViewAdapter.notifyDataSetChanged();
+                                toastShow("已删除");
+                            }
+                        })
+                        .setNeutralButton("取消", null)
+                        .create()
+                        .show();
+                return true;
+            }
+        });
+    }
+
+    /**
+     * 显示消息
+     *
+     * @param message 消息
+     */
+    private void toastShow(String message)
+    {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
